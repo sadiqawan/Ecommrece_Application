e61f5b8dea7e862e527e1381cc4ab14fca177bd0
sadiqawan <awansadiq09@gmail.com>
2024-03-10 13:28:05 +0500

    working on customer home

---
+++ lib/controls/providers/customer_home_provider.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+
+class CustomerHomeProvider extends ChangeNotifier{
+
+  Future <void> lableData()async {
+
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+
+  }
+
+
+}
--- lib/views/customer_home_screen.dart
+++ lib/views/customer_home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CustomerHomeScreen extends StatefulWidget {
   const CustomerHomeScreen({super.key});
@@ -10,6 +13,87 @@ class CustomerHomeScreen extends StatefulWidget {
 class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.person_outlined))
+        ],
+        title: const Text(
+          'bagzz',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(
+        children: [
+          CarouselSlider(
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(color: Colors.amber),
+                      child: Text(
+                        'text $i',
+                        style: TextStyle(fontSize: 16.0),
+                      ));
+                },
+              );
+            }).toList(),
+            options: CarouselOptions(
+              height: 340,
+              aspectRatio: 16 / 9,
+              viewportFraction: 0.8,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayAnimationDuration: const Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              enlargeFactor: 0.3,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+
+            child: GridView.count(
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              children: [
+                _buildGridItem(),
+                _buildGridItem(),
+                _buildGridItem(),
+                _buildGridItem(),
+              ],
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGridItem() {
+    return Container(
+      color: Color(0xFFF1F1F1),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('images/icon_image.jpg', height: 100),
+          Text('name'),
+          Text('shopNow'),
+        ],
+      ),
+    );
   }
 }
