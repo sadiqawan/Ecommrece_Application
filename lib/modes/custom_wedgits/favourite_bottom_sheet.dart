eea051239a36a63d4879b61c7fbf19f19cca2b7d
sadiqawan <awansadiq09@gmail.com>
2024-03-14 15:05:44 +0500

    working on  bottom sheet

---
+++ lib/modes/custom_wedgits/favourite_bottom_sheet.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class FavouriteBottomSheet extends StatefulWidget {
+  final BuildContext context;
+
+  const FavouriteBottomSheet({super.key, required this.context});
+
+  @override
+  State<FavouriteBottomSheet> createState() => _FavouriteBottomSheetState();
+}
+
+class _FavouriteBottomSheetState extends State<FavouriteBottomSheet> {
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: widget.context,
+      builder: (BuildContext bc) {
+        return Container(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text('Option 1'),
+                onTap: () {
+                  // Add your functionality here
+                  Navigator.pop(context); // Close the bottom sheet
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text('Option 2'),
+                onTap: () {
+                  // Add your functionality here
+                  Navigator.pop(context); // Close the bottom sheet
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // You can return any Widget here
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _showBottomSheet(); // Call the method to show the bottom sheet when the widget is initialized
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:ecommrece_application/views/customer_home_screen.dart';
 import 'package:flutter/material.dart';
-
 import 'customer_favourite_screen.dart';
 import 'customer_search_screen.dart';
 import 'customer_shopping_screen.dart';
@@ -19,11 +18,9 @@ class _HomeScreenState extends State<HomeScreen> {
     const CustomerSearchScreen(),
     const CustomerFavoriteScreen(),
     const CustomerShoppingScreen(),
-
-
-
   ];
   int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,6 +49,17 @@ class _HomeScreenState extends State<HomeScreen> {
         onTap: (index) {
           setState(() {
             _currentIndex = index;
+            if (index == 2) {
+              // If favorite icon is clicked
+              favouriteShowBottomSheet(context);
+            }
+          });
+          setState(() {
+            _currentIndex = index;
+            if (index == 4) {
+              // If favorite icon is clicked
+              cardShowBottomSheet(context);
+            }
           });
         },
       ),
@@ -59,4 +67,50 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
+  void favouriteShowBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return Container(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text('Option 1'),
+                onTap: () => {}, // Add your functionality here
+              ),
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text('Option 2'),
+                onTap: () => {}, // Add your functionality here
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+  void cardShowBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return Container(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text('items no 1'),
+                onTap: () => {}, // Add your functionality here
+              ),
+              ListTile(
+                leading: Icon(Icons.album),
+                title: Text('Item No 2'),
+                onTap: () => {}, // Add your functionality here
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
