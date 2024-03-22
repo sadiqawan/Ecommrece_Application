2f0aa42b5734c1edf6a698f9d2c1142e6c9e8b0a
sadiqawan <awansadiq09@gmail.com>
2024-03-23 01:21:55 +0500

    profileScreen done

--- lib/modes/custom_wedgits/profile_show_name_container.dart
+++ lib/modes/custom_wedgits/profile_show_name_container.dart
@@ -8,17 +8,21 @@ class ProfileNameContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15.0),
-      child: Expanded(
-        child: Container(
-          height: 50,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              border: Border.all(color: Colors.black)),
-          child: Align( alignment: Alignment.centerLeft, child: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-          )),
-        ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.black)),
+              child: Align( alignment: Alignment.centerLeft, child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              )),
+            ),
+          ),
+        ],
       ),
     );
   }
