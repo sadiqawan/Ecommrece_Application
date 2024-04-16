8d46be1c3d2a23ca328b0569a6231fb7d26495e8
sadiqawan <awansadiq09@gmail.com>
2024-04-16 16:52:21 +0500

    categ screen woke

--- lib/views/all_categries_list_screen.dart
+++ lib/views/all_categries_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ecommrece_application/controls/providers/customer_home_provider.dart';
+import 'package:ecommrece_application/views/categori_detail_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -29,38 +30,68 @@ class _AllCategoriesListScreenState extends State<AllCategoriesListScreen> {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.all(15.0),
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 25.0),
-                      child: Container(
-                        height: 250,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black45,
-                              blurRadius: 4,
-                              spreadRadius: 2,
-                              offset: Offset(4, 8), // Shadow position
+                child: InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CategoriesDetailScreen(
+                            image: value.brands![index]['image'],
+                            name: value.brands![index]['name'],
+                            description: ''
+                            // value.brands![index]['description']
+                        )));
+                  },
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 25.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 250,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black45,
+                                    blurRadius: 4,
+                                    spreadRadius: 2,
+                                    offset: Offset(4, 8), // Shadow position
+                                  ),
+                                ],
+                                color: Colors.black12.withOpacity(0.4),
+                              ),
                             ),
                           ],
-                          color: Colors.black12.withOpacity(0.4),
                         ),
                       ),
-                    ),
-                    Positioned.fill(
-                      bottom: 30.0,
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: Image.network(
-                          value.brands![index]['image'].toString(),
-                          height: 200,
+                      Positioned.fill(
+                        bottom: 40.0,
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Image.network(
+                            value.brands![index]['image'].toString(),
+                            height: 200,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      Positioned(
+                        bottom: 20.0,
+                        left: 0,
+                        right: 0,
+                        child: Text(
+                          value.brands![index]['name'],
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               );
             },
